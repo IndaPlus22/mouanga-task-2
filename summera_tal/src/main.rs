248606a315c23b234325b165f392e403ec56f155
salticecream <shortgolind@gmail.com>
2022-09-16 00:01:04 +0200

    logic

--- summera_tal/src/main.rs
+++ summera_tal/src/main.rs
@@ -4,7 +4,7 @@
 *
 *	Kattis I/O functionality partly based on code from: 		https://open.kattis.com/help/rust
 														   		https://github.com/IndaPlus22/AssignmentInstructions-BlueNote/blob/main/task-2/minimal_scalar_product/src/main.rs
-																https://www.youtube.com/watch?v=JkJxRn1OnWA&t=350s
+																https://www.youtube.com/watch?v=JkJxRn1OnWA
 */
 use std::io;
 use std::io::prelude::*;
@@ -17,8 +17,13 @@ fn main() {
 	.read_line(&mut amount_of_numbers)
 	.expect( ""); // Cause thep rogram crashes if you dont have this!
 
-	let amount_of_numbers:u32 = amount_of_numbers.trim().parse().expect(""); 
+	let mut amount_of_numbers:u32 = amount_of_numbers
+	.trim()
+	.parse()
+	.expect(""); 
+	let mut amount_to_add = (amount_of_numbers + 1)/2; // integer divison baby!
 
-	eprintln!("Du vill ha: {}", amount_of_numbers - 4); // same as above comment
+	eprintln!("Du vill ha: {}", amount_of_numbers); // same as above comment
+	eprintln!("Antalet tal som kommer adderas är: {}", amount_to_add);
 
 }
