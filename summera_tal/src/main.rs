1ab0dbd67173d1b6dfccba9f35fe7c6900f998ba
salticecream <shortgolind@gmail.com>
2022-09-15 22:44:37 +0200

    some i/o...

--- summera_tal/src/main.rs
+++ summera_tal/src/main.rs
@@ -2,12 +2,17 @@
 *	Solution to Inda+22 Task 2
 *	By: Anders Mouanga (mouanga@kth.se)
 *
-*	Kattis I/O functionality partly based on code from https://open.kattis.com/help/rust
-*/
+*	Kattis I/O functionality partly based on code from: 		https://open.kattis.com/help/rust
+														   		https://github.com/IndaPlus22/AssignmentInstructions-BlueNote/blob/main/task-2/minimal_scalar_product/src/main.rs
 
-use std::io::{self, BufRead};
+*/
+use std::io;
+use std::io::prelude::*;
 
 fn main() {
-	let stdin = io::stdin();
-}
+	let mut amount_of_numbers = String::new();
 
+    println!("Hur många tal?");
+	io::stdin().read_line(&mut amount_of_numbers).unwrap();
+	println!("Du vill ha: {}", amount_of_numbers);
+}
