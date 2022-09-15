f06140f5df0d4077a852637f950bd8f19d8ad847
salticecream <shortgolind@gmail.com>
2022-09-15 23:48:12 +0200

    fix some i/o. rust sucks

--- summera_tal/src/main.rs
+++ summera_tal/src/main.rs
@@ -4,7 +4,7 @@
 *
 *	Kattis I/O functionality partly based on code from: 		https://open.kattis.com/help/rust
 														   		https://github.com/IndaPlus22/AssignmentInstructions-BlueNote/blob/main/task-2/minimal_scalar_product/src/main.rs
-
+																https://www.youtube.com/watch?v=JkJxRn1OnWA&t=350s
 */
 use std::io;
 use std::io::prelude::*;
@@ -12,7 +12,13 @@ use std::io::prelude::*;
 fn main() {
 	let mut amount_of_numbers = String::new();
 
-    println!("Hur många tal?");
-	io::stdin().read_line(&mut amount_of_numbers).unwrap();
-	println!("Du vill ha: {}", amount_of_numbers);
+    eprintln!("Hur många tal?"); // remove later for faster runtime
+	io::stdin()
+	.read_line(&mut amount_of_numbers)
+	.expect( ""); // Cause thep rogram crashes if you dont have this!
+
+	let amount_of_numbers:u32 = amount_of_numbers.trim().parse().expect(""); 
+
+	eprintln!("Du vill ha: {}", amount_of_numbers - 4); // same as above comment
+
 }
